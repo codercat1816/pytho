4732226adef6cce0bb24f1eae45b7a37350cd1ec
codercat1816 <165159391+codercat1816@users.noreply.github.com>
2025-03-04 20:54:21 +0000

    Update fibonacci.py

--- fibonacci.py
+++ fibonacci.py
@@ -1,14 +1,14 @@
 terms=int(input("number of terms: "))
 n1=0,n2=1
 count=0
- if terms <= 0:
-  print("enter a positive integer")
- elif terms == 1:
+if terms <= 0:
+  print("enter a positive integer:")
+elif terms == 1:
   print("fibonacci sequence upto",terms,":")
   print(n1)
- else:
-  print ("fibonacci sequence :")
-   while count < terms:
+else:
+  print ("fibonacci sequence:")
+while count < terms:
     print(n1)
      n=n1+n2
      n1=n2
